2b1a2bccba38f690d73621706311dbb812c883cc
enock24 <enookimuu@gmail.com>
2021-01-16 12:39:52 +0300

    add login views

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    images=Image.objects.all()
+    comments=Comments.objects.all()
+    return render(request,'instagram/index.html',{"images":images,"comments":comments})
+
