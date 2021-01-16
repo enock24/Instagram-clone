625551baec5f286da55242f07dcd571056a51ed2
enock24 <enookimuu@gmail.com>
2021-01-16 12:53:59 +0300

    add post view

--- instagram/views.py
+++ instagram/views.py
@@ -40,4 +40,63 @@ def upload_image(request):
     else:
         form = ImageForm()
         return render(request,'instagram/upload_image.html', {"form":form})
+
+
+@login_required (login_url='/accounts/register/')          
+def image_likes(request,id):
+    image =  Image.get_single_photo(id)
+    user = request.user
+    user_id = user.id
+    
+    if user.is_authenticated:
+    
+        image.save()
+        
+    return redirect('index')
+
+def add_comment(request,id):
+    current_user = request.user
+    image = Image.get_single_photo(id=id)
+    if request.method == 'POST':
+        form = CommentsForm(request.POST)
+        print(form)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = current_user
+            comment.image_id = id
+            comment.save()
+        return redirect('index')
+
+    else:
+        form = CommentsForm()
+        return render(request,'instagram/add_comment.html',{"form":form,"image":image})  
+    
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+        return redirect('profile')
+
+    else:
+        form = ProfileForm()
+        return render(request,'registration/edit_profile.html',{"form":form})
+    
+    
+def create_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+        return redirect('index')
+
+    else:
+        form = ImageForm()
+        return render(request,'instagram/new_post.html',{"form":form})
+    
     
